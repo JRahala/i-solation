bba2aae5ec8597ca58c72bb1829b8df8c83675b8
Jamhead20000 <45700318+Jamhead20000@users.noreply.github.com>
2020-06-02 21:51:27 +0100

    Template inheritance basics
    Login + Register files added in as well.
    {{set fileName}} system inplace. Will not be used much for login / registration pages in final product.
    
    Also added notes page for stuff I need to do / reminders

--- main.py
+++ main.py
@@ -1,36 +1,19 @@
 from classes import *
 
-
-myWebsite = Website()
-
-Jasamrit = User('Jasamrit', 'Rahala')
-registered_user = myWebsite.register_user(Jasamrit)
-
-print(registered_user.username, registered_user.password)
-
-Jasamrit.create_post('Header', 'Content')
-Jasamrit.delete_post('Header')
-
-Jasamrit.create_post('A', 'B')
-
-Jasamrit.edit_post('A', 'B', 'C')
-
-print(Jasamrit.posts)
-print(Jasamrit.posts['B'])
-
-
-'''
 from flask import Flask, request, render_template, jsonify
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-
 @app.route('/')
-def index():
-	return '<h> Index test </h>'
+def register():
+	return render_template('register.html')
 
 
-socketio.run(app, host = '0.0.0.0', port = 5000)
-'''
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
+
+socketio.run(app, host = '0.0.0.0', port = 4000, debug = True)
