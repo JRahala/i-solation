368188a1ceb6974ca51654b517eb20b8e747be1f
Jamhead20000 <45700318+Jamhead20000@users.noreply.github.com>
2020-06-02 19:57:34 +0100

    Post class functionality
    Posts can be created, deleted and editted from the user class.
    
    Essential functionality built

--- classes.py
+++ classes.py
@@ -1,4 +1,7 @@
 
+import time
+
+
 class Website:
 
 	def __init__(self):
@@ -6,26 +9,96 @@ class Website:
 		self.database = {}
 
 
-	def register_user(self):
+	def register_user(self, user):
 
-		pass
+		if not user.username in self.database:
 
+			self.database[user.username] = [user.password, user]
+			return user
 
-	def login_user(self):
+		return False
+		
 
-		pass
+	def login_user(self, username, password):
+
+		if username in self.database:
+
+			if self.database[username][0] == password:
+				return self.database[username][1]
+
+		return False
+
 
 
 class Post:
 
-	def __init__(self):
+	def __init__(self, heading, content, author):
 
-		pass
+		self.heading = heading
+		self.content = content
+		self.author = author
 
-		
+		self.votes = 0
+		self.date = time.localtime()
+		self.comment = set()
+
+
+	def edit(self, new_heading, new_content):
+
+		if new_heading != self.heading:
+
+			if new_heading in self.author.posts: 
+				return False
+
+			self.author.posts[new_heading] = self.author.posts[self.heading]
+			del self.author.posts[self.heading]
+
+		self.heading = new_heading
+		self.content = new_content
+
+		return self
+
+
 class User:
 
 	def __init__(self, username, password):
 
 		self.username = username
 		self.password = password
+
+		self.posts = {}
+		self.reputation = 0
+
+
+	def create_post(self, heading, content):
+
+		if not heading in self.posts:
+
+			post = Post(heading, content, self)
+			self.posts[post.heading] = post
+
+			return post
+
+		return False
+
+
+	def delete_post(self, heading):
+
+		if heading in self.posts:
+
+			post = self.posts[heading]
+			del self.posts[heading], post
+
+			return True
+
+		return False
+
+
+	def edit_post(self, heading, new_heading = False, new_content = False):
+
+		post = self.posts[heading]
+
+		if not new_heading: new_heading = post.heading
+		if not new_content: new_content = post.content
+		
+		return post.edit(new_heading, new_content)
--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from classes import *
 
+
+myWebsite = Website()
+
+Jasamrit = User('Jasamrit', 'Rahala')
+registered_user = myWebsite.register_user(Jasamrit)
+
+print(registered_user.username, registered_user.password)
+
+Jasamrit.create_post('Header', 'Content')
+Jasamrit.delete_post('Header')
+
+Jasamrit.create_post('A', 'B')
+
+Jasamrit.edit_post('A', 'B', 'C')
+
+print(Jasamrit.posts)
+print(Jasamrit.posts['B'])
+
+
+'''
 from flask import Flask, request, render_template, jsonify
 from flask_socketio import SocketIO
 
@@ -13,3 +33,4 @@ def index():
 
 
 socketio.run(app, host = '0.0.0.0', port = 5000)
+'''
