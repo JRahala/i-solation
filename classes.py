bbdf1a76a3e8cf6578475882f112e311f4bfccca
Jamhead20000 <45700318+Jamhead20000@users.noreply.github.com>
2020-06-02 18:27:04 +0100

    Website framework
    Routing in main.py and socketio(app). Added empty classes in classes.py

--- classes.py
+++ classes.py
@@ -1,9 +1,31 @@
 
+class Website:
 
+	def __init__(self):
+
+		self.database = {}
+
+
+	def register_user(self):
+
+		pass
+
+
+	def login_user(self):
+
+		pass
+
+
+class Post:
+
+	def __init__(self):
+
+		pass
+
+		
 class User:
 
 	def __init__(self, username, password):
 
 		self.username = username
 		self.password = password
-
--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 from classes import *
 
+from flask import Flask, request, render_template, jsonify
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+
+@app.route('/')
+def index():
+	return '<h> Index test </h>'
+
+
+socketio.run(app, host = '0.0.0.0', port = 5000)
